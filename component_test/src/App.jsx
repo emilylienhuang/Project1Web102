a53acb61b3167717428cc1d2cedba7ed9e281d29
emilylienhuang <eng@uccs.edu>
2025-06-10 12:22:11 -0600

    working porject

--- component_test/src/App.jsx
+++ component_test/src/App.jsx
@@ -1,23 +1,24 @@
 import './App.css';
 import Card from './components/Card'
+
 const App = () => {
-  const cardData = [{title: "Optimizing Caching Workloads with Kubernetes", description: "Implementing a working microservice architecture using Kubernetes and Docker Networks", link:"https://github.com/emilylienhuang/YOURLSKubernetesAndNetworks"},
-    {title:"Productivity Optimization", description:"An application with an algorithmic optimization approach to productivity usign Spoon Theory.", link: "https://github.com/emilylienhuang/SpoonTheory"},
-    {title: "AI/ML in IoT Devices", description: "Using AI/ML plus a full tech stack to implement interactive lighting schemes.", link: "https://github.com/emilylienhuang/AILightbulb"},
-    {title: "Poetry Generation using Natural Languag Processing", description: "Generating N-gram poems across 5 languages with high reading ease scores and of 5th grader level comprehension.", link: "https://github.com/emilylienhuang/PoetryBot"},
-    {title: "ML Algorithms for Big Data Classification and Clustering", description: "Testing machine learning algorithms to classify and cluster big data.", link: "https://github.com/emilylienhuang/MLProjects"},
-    {title: "Consensus Dynamics in Network Game Modeling", description: "Modeling group behavior through consensus dynamics.", link:"https://github.com/emilylienhuang/GroupThink"},
-    {title: "LRU versus LFU cache optimization", description: "Modeling cache optimization tradeoffs between an LRU and an LFU implementation", link: "https://github.com/emilylienhuang/LRU_LFUOptimization"},
-    {title: "Embedded ticketing system", description: "Modeling an embedded system ticketing system in C using a variety of data structures and creating personal libraries.", link: "https://github.com/emilylienhuang/CProjects"},
-    {title: "Algorithmic Exploration, Basic", description: "An in-depth exploration of algorithms and their optimizations ranging from simple sorting to Dynamic Programming and NP-Hard problems.", link: "https://github.com/emilylienhuang/AlgorithmExplorationsPartOne"},
-    {title: "Leetcode Practice", description: "Track how I practice leetcode every day.", link: "https://github.com/emilylienhuang/leetcode"}
+  const cardData = [{title: "Optimizing Caching Workloads with Kubernetes", description: "Implementing a working microservice architecture using Kubernetes and Docker Networks", link:"https://github.com/emilylienhuang/YOURLSKubernetesAndNetworks", image: "kube.png"},
+    {title:"Productivity Optimization", description:"An application with an algorithmic optimization approach to productivity usign Spoon Theory.", link: "https://github.com/emilylienhuang/SpoonTheory", image:"spoon.jpeg"},
+    {title: "AI/ML in IoT Devices", description: "Using AI/ML plus a full tech stack to implement interactive lighting schemes.", link: "https://github.com/emilylienhuang/AILightbulb", image:"bulb.png"},
+    {title: "Poetry Generation using Natural Languag Processing", description: "Generating N-gram poems across 5 languages with high reading ease scores and of 5th grader level comprehension.", link: "https://github.com/emilylienhuang/PoetryBot", image: "poetry.jpeg"},
+    {title: "ML Algorithms for Big Data Classification and Clustering", description: "Testing machine learning algorithms to classify and cluster big data.", link: "https://github.com/emilylienhuang/MLProjects", image:"bigdata.jpeg"},
+    {title: "Consensus Dynamics in Network Game Modeling", description: "Modeling group behavior through consensus dynamics.", link:"https://github.com/emilylienhuang/GroupThink", image:"groupthink.png"},
+    {title: "LRU versus LFU cache optimization", description: "Modeling cache optimization tradeoffs between an LRU and an LFU implementation", link: "https://github.com/emilylienhuang/LRU_LFUOptimization", image:"cache.jpeg"},
+    {title: "Embedded ticketing system", description: "Modeling an embedded system ticketing system in C using a variety of data structures and creating personal libraries.", link: "https://github.com/emilylienhuang/CProjects", image:"ticket.png"},
+    {title: "Algorithmic Exploration, Basic", description: "An in-depth exploration of algorithms and their optimizations ranging from simple sorting to Dynamic Programming and NP-Hard problems.", link: "https://github.com/emilylienhuang/AlgorithmExplorationsPartOne", image:"dijkstras.png"},
+    {title: "Leetcode Practice", description: "Track how I practice leetcode every day.", link: "https://github.com/emilylienhuang/leetcode", image:"leetcode.png"}
   ]
   return (
     <div className="App">
       <h1> Emily Ng's Project Work </h1>
       <div className="card-container">
       {cardData.map((card, index) => (
-        <Card key={index} title={card.title} description={card.description} link={card.link} />
+        <Card key={index} title={card.title} description={card.description} link={card.link} image={card.image} />
       ))}
       </div>
     </div>
--- component_test/src/components/Card.jsx
+++ component_test/src/components/Card.jsx
@@ -2,6 +2,7 @@ const Card = (props) => {
     return (
         <div className="card">
             <h3 className="card-title" >{props.title}</h3>
+            <img src={`/images/${props.image}`} alt="photo" className="card-image"/>
             <p className="card-desc"> {props.description}</p>
             <a  className = "cardButton" href={props.link} target="_blank"
         rel="noopener noreferrer">Visit Repo</a>
